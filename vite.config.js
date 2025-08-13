fedd4603cb9825ccd7a3f50b461b59a53280db9c
kill0086 <klii0086@student.monash.edu>
2025-08-13 15:58:09 +0800

    Trigger deployment

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,13 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
-module.exports = {
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/week11/'
-    : '/'
-}
+
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: process.env.NODE_ENV === 'production'
+    ? '/week11/' // 生产环境部署的路径
+    : '/',
   plugins: [
     vue(),
     vueDevTools(),
@@ -20,4 +18,3 @@ export default defineConfig({
     }
   }
 })
-
